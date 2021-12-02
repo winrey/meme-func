93060d4fc23724597ecac720b4fdd5a6d86c7ba1
Winrey <i@iaside.com>
2021-12-02 16:35:48 +0800

    impore: add await for init

--- src/init.ts
+++ src/init.ts
@@ -1,3 +1,4 @@
+import { readSync } from 'fs';
 import { catchError } from './errors/catch';
 import { initLogger } from './logger';
 import { Service, serviceSelector } from './mvc/service';
@@ -5,9 +6,25 @@ import { CloudInputArgumentType } from './typings/args';
 import { initContext, setInstanceDebug } from './utils/getContext';
 import { wxCloudInit } from './utils/wx-cloud';
 
+let waiting: any[] = []
+let isReady = false
+
+const ready = () => {
+  return new Promise<void>(resolve => {
+    if (isReady) {
+      resolve()
+      return
+    }
+    waiting.push(resolve)
+  })
+}
+
 export const initMeme = async ({ isDebug, env }: { isDebug?: boolean; env?: symbol | string } = {}) => {
   setInstanceDebug(isDebug);
   wxCloudInit({ env });
+  isReady = true
+  waiting.forEach(r => setImmediate(() => r()))
+  waiting = []
 };
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -20,9 +37,10 @@ export const handleMemeReq = async ({
   isDebug?: boolean;
   srvs?: Record<string, Service>;
 }) => {
+  await ready()
   return await initContext({ request, isDebug }, async () => {
-    return await initLogger(request.event, request.context, async () => {
-      return await catchError(async () => {
+    return await catchError(async () => {
+      return await initLogger(request.event, request.context, async () => {
         return await serviceSelector(
           {
             event: request.event,
